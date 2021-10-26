77bde8c0f1ef98c9888ab7be9566b572850142ec
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-26 09:17:13 +0530

    Added:Welcome to user

---
+++ src/main/java/com/bridgelabz/hotelreservationsystems/HotelReservation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservationsystems;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome Hotel Reservation System");
+	 
+	}
+
+}
